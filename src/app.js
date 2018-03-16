c475c7eaf73d1573289fc45a3c95cff2a353cab5
Shota Kawashima <bysk.333@gmail.com>
2018-03-17 01:20:07 +0900

    スタイル使いまわして一応完成

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 import { h, app } from 'hyperapp' // eslint-disable-line
+import marked from 'marked'
+
+import './style.sass'
 
 const state = {
-  'input': 'hello world',
+  'input': '# markdown',
   'output': ''
 }
 
 const actions = {
-  setInput: (input) => state => ({ input: input })
+  setInput: (input) => state => ({ output: marked(input) })
 }
 
 const view = (state, actions) => (
-  <main>
-    <textarea id='editor' oninput={e => actions.setInput(document.getElementById('editor').value)} />
-    <div innerHTML={state.input}></div>
+  <main id='app'>
+    <textarea id='editor' oninput={e => actions.setInput(document.getElementById('editor').value)} placeholder={state.input} />
+    <div id='preview' innerHTML={state.output}></div>
   </main>
 )
 
