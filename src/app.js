76ed7f4cc5d915fa271555fc0042e42c67aceb60
Shota Kawashima <bysk.333@gmail.com>
2018-03-17 00:47:04 +0900

    textareaの中身をこんそろぐ

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ const actions = {
 }
 
 const view = (state, actions) => (
-  <h1>{state.input}</h1>
+  <main>
+    <h1>{state.input}</h1>
+    <textarea id='editor' oninput={e => console.log(document.getElementById('editor').value)} />
+  </main>
 )
 
 app(state, actions, view, document.body)
+// <textarea id='editor' onclick={e => addTodo(document.querySelector('#todo-val').value)
